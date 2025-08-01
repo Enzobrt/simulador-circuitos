e5d5b89676c475ea7f129b4eee3e0757b441f349
enzo <enzobroton@gmail.com>
2025-08-01 20:57:00 +0200

    añadir tests

--- test_paralelo.py
+++ test_paralelo.py
@@ -2,8 +2,19 @@
 
 from componentes import Resistencia, CircuitoParalelo
 
-resistencias = [Resistencia(9), Resistencia(18), Resistencia(30)]
+def test_sencillo():
+    # Test para un circuito sencillo con una sola resistencia
+    resistencias = [Resistencia(30)]
+    c = CircuitoParalelo(resistencias, 6)
+    assert c.calcular_resistencia() == 30
+    assert c.calcular_voltaje() == 6
+    assert c.calcular_corriente() == 0.2
 
-c = CircuitoParalelo(resistencias, 90)
+def test_varias_r():
+    # Test para un circuito con varias resistencias
+    resistencias = [Resistencia(10), Resistencia(15), Resistencia(30)]
+    c = CircuitoParalelo(resistencias, 90)
+    assert c.calcular_resistencia() == 5
+    assert c.calcular_voltaje() == 90
+    assert c.calcular_corriente() == 18
 
-print(c)
--- test_serie.py
+++ test_serie.py
@@ -2,8 +2,19 @@
 
 from componentes import Resistencia, CircuitoSerie
 
-resistencias = [Resistencia(20), Resistencia(60), Resistencia(40)]
+def test_sencillo():
+    # Test para un circuito sencillo con una sola resistencia
+    resistencias = [Resistencia(30)]
+    c = CircuitoSerie(resistencias, 6)
+    assert c.calcular_resistencia() == 30
+    assert c.calcular_voltaje() == 6
+    assert c.calcular_corriente() == 0.2
 
-c = CircuitoSerie(resistencias, 6)
 
-print(c)
+def test_varias_r():
+    # Test para un circuito con varias resistencias
+    resistencias = [Resistencia(20), Resistencia(40), Resistencia(60)]
+    c = CircuitoSerie(resistencias, 6)
+    assert c.calcular_resistencia() == 120
+    assert c.calcular_voltaje() == 6
+    assert c.calcular_corriente() == 0.05
